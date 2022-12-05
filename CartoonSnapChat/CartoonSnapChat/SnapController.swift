ca3e67d10eeb70ed2e3207660672809adb59684d
Ali Eldeeb <aeldeeb@fordham.edu>
2022-12-05 10:50:57 -0800

    Setup project directory

---
+++ CartoonSnapChat/CartoonSnapChat/SnapController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CartoonSnapChat
+//
+//  Created by Ali Eldeeb on 12/5/22.
+//
+
+import UIKit
+
+class SnapController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
